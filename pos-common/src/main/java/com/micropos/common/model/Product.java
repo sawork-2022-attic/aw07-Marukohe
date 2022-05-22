8dd0957aebfb58c1050350724dd09b20fc3d2759
Wei He <marukohe@qq.com>
2022-05-22 17:36:50 +0800

    add gateway, discovery, products module; connect product repository with mongodb

--- pos-common/src/main/java/com/micropos/common/model/Product.java
+++ pos-common/src/main/java/com/micropos/common/model/Product.java
@@ -8,6 +8,13 @@ public class Product implements Serializable {
     private double price;
     private String image;
 
+    public Product(String id, String name, double price, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
     public String getId() {
         return id;
     }
